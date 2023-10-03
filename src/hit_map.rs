41fa78e9a8ca6f87dc141ea063099a6b7f7df076
Zerthox <zerthox@gmail.com>
2023-10-03 13:24:30 +0200

    Use initial weapon set

--- src/hit_map.rs
+++ src/hit_map.rs
@@ -1,18 +1,18 @@
 use crate::{Hit, HitWithSkill, Skill, Time, WeaponMap, WeaponSet};
 use arcdps_parse::{CombatEvent, EventKind, Log};
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, iter};
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct WeaponSetHits {
-    pub set: Option<WeaponSet>,
+    pub set: WeaponSet,
     pub damage: i64,
     pub hit_count: usize,
     pub hits: Vec<HitWithSkill>,
 }
 
 impl WeaponSetHits {
-    pub fn new(set: Option<WeaponSet>, hits: impl IntoIterator<Item = HitWithSkill>) -> Self {
+    pub fn new(set: WeaponSet, hits: impl IntoIterator<Item = HitWithSkill>) -> Self {
         let hits: Vec<_> = hits.into_iter().collect();
         Self {
             set,
@@ -30,26 +30,20 @@ pub fn map_hits_to_set<'a>(
 ) -> impl Iterator<Item = WeaponSetHits> {
     let start = Time::log_start(log);
     let mut sets: HashMap<WeaponSet, Vec<HitWithSkill>> = HashMap::new();
-    let mut unknown = Vec::new();
-
     let weapons = WeaponMap::new(&log.events, agent);
 
     for event in events {
-        if event.kind() == EventKind::DirectDamage && event.src_agent == agent {
+        if event.kind() == EventKind::DirectDamage {
             let new = HitWithSkill {
                 skill: Skill::from_log(log, event.skill_id),
                 hit: Hit::try_from_event(log, event, start.relative(event.time)).unwrap(),
             };
 
-            match weapons.set_at(event.time) {
-                None => unknown.push(new),
-                Some(set) => sets.entry(set).or_default().push(new),
-            }
+            let set = weapons.set_at(event.time).unwrap_or(WeaponSet::Initial);
+            sets.entry(set).or_default().push(new);
         }
     }
 
-    iter::once(WeaponSetHits::new(None, unknown)).chain(
-        sets.into_iter()
-            .map(|(set, hits)| WeaponSetHits::new(Some(set), hits)),
-    )
+    sets.into_iter()
+        .map(|(set, hits)| WeaponSetHits::new(set, hits))
 }
