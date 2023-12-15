b4accb27cafbb638bfa4a8dbec41c2c521b8be08
Zerthox <zerthox@gmail.com>
2023-12-15 15:29:13 +0100

    Update effect struct

--- src/effect.rs
+++ src/effect.rs
@@ -1,5 +1,8 @@
 use crate::{Agent, Time};
-use evtc_parse::{self as evtc, effect::ContentLocal, Event, Log, Position};
+use evtc_parse::{
+    effect::{ContentLocal, Effect as RawEffect, EffectGUID, EffectLocation},
+    Event, Log, Position,
+};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -10,6 +13,7 @@ pub struct Effect {
     #[serde(flatten)]
     pub info: Option<EffectInfo>,
     pub owner: Option<Agent>,
+    pub moving_platform: u8,
     pub location: EffectLocation,
     pub duration: u32,
     pub tracking_id: u32,
@@ -22,17 +26,11 @@ pub struct EffectInfo {
     pub content_local: Option<ContentLocal>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum EffectLocation {
-    Agent(Agent),
-    Position(Position),
-}
-
-impl EffectLocation {
-    pub fn from_location(location: evtc::effect::EffectLocation, log: &Log) -> Self {
-        match location {
-            evtc::effect::EffectLocation::Agent(id) => Self::Agent(Agent::from_log(id, log)),
-            evtc::effect::EffectLocation::Position(pos) => Self::Position(pos),
+impl From<EffectGUID> for EffectInfo {
+    fn from(event: EffectGUID) -> Self {
+        Self {
+            guid: event.guid_string(),
+            content_local: event.content_local,
         }
     }
 }
@@ -44,37 +42,29 @@ pub fn extract_effects<'a>(
     let start = Time::log_start(log);
     let guids: HashMap<_, _> = events
         .clone()
-        .filter_map(|event| event.try_extract::<evtc::effect::EffectGUID>())
-        .map(|event| {
-            (
-                event.effect_id,
-                EffectInfo {
-                    guid: event.guid_string(),
-                    content_local: event.content_local,
-                },
-            )
-        })
+        .filter_map(|event| event.try_extract::<EffectGUID>())
+        .map(|event| (event.effect_id, event.into()))
         .collect();
 
     events
-        .filter_map(|event| {
-            event.try_extract::<evtc::effect::Effect>().map(|effect| {
-                let info = guids.get(&effect.effect_id).cloned();
-                Effect {
-                    time: start.relative(event.time),
-                    effect_id: effect.effect_id,
-                    info,
-                    owner: if effect.owner != 0 {
-                        Some(Agent::from_log(effect.owner, log))
-                    } else {
-                        None
-                    },
-                    location: EffectLocation::from_location(effect.location, log),
-                    duration: effect.duration,
-                    tracking_id: effect.tracking_id,
-                    orientation: effect.orientation.into(),
-                }
-            })
+        .filter_map(|event| event.try_extract::<RawEffect>())
+        .map(|effect| {
+            let info = guids.get(&effect.effect_id).cloned();
+            Effect {
+                time: start.relative(effect.time),
+                effect_id: effect.effect_id,
+                info,
+                owner: if effect.owner != 0 {
+                    Some(Agent::from_log(effect.owner, log))
+                } else {
+                    None
+                },
+                moving_platform: effect.moving_platform,
+                location: effect.location,
+                duration: effect.duration,
+                tracking_id: effect.tracking_id,
+                orientation: effect.orientation.into(),
+            }
         })
         .collect()
 }
